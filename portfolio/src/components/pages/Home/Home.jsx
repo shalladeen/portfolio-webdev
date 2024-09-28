de5a297f852b5bf45bd25e7f2d46c22ef6c53fae
Shawna <shawnakatrina.halladeen@gmail.com>
2024-09-28 17:26:39 -0400

    updated home page and moved other pages into folders

--- portfolio/src/components/pages/Home/Home.jsx
+++ portfolio/src/components/pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {FiArrowDown} from 'react-icons/fi';
 import './Home.css';
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
             <h1>Hi, I'm Shawna</h1>
             <p>I'm a software engineering student</p>
             <a href="projects" className="view-projects-button">View Projects
-                <FiArrowDown className="arrow-icon" />
             </a>
                 
         </div>
